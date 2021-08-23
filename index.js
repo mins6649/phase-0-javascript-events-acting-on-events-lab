c779152fdd9a7a188d8a8766a5a350d97b75bbc2
Minyoung Shin <mins6649@gmail.com>
2021-08-23 12:39:07 -0400

    moving elememts horizontally

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // Your code here
-var dodger = document.getElementById('dodger');
+const dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10)
+ 
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
-  if (left < 360) {
-    dodger.style.left = `${left + 1}px`
+  const rightNumbers = dodger.style.left.replace('px', '');
+  const right = parseInt(rightNumbers, 10)
+
+  if (right < 400) {
+    dodger.style.left = `${right + 1}px`
   }
 }
 
